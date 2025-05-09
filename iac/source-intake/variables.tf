7116319df3321030822f09d56c4fb75be49d9b81
francisco laptop <franserr2288@outlook.com>
2025-05-08 18:18:45 -0700

    source-intake iac scaffolding, need to double check everything and make adjustments. once that is done, i'll start using localstack to iterate locally

---
+++ iac/source-intake/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name of the shared intake S3 bucket"
+}
+
+variable "bucket_arn" {
+  type        = string
+  description = "ARN of the shared intake S3 bucket"
+}
